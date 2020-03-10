8d5a0365e2769174be682a376abfa29ac5034e82
Xiaobo He <hexiaobo001@hotmail.com>
2020-03-10 16:20:58 -0400

    output

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -2,7 +2,7 @@
 * @Author: hexia
 * @Date:   2020-01-17 17:23:30
 * @Last Modified by:   hexia
-* @Last Modified time: 2020-03-10 16:17:10
+* @Last Modified time: 2020-03-10 16:20:46
 */
 
 public class NBody  {
@@ -90,12 +90,12 @@ public class NBody  {
 
 		    }
 
-		    StdOut.printf("%d\n", planets.length);
+		    StdOut.printf("%d\n", bodies.length);
             StdOut.printf("%.2e\n", radius);
             for (int i = 0; i < planets.length; i++) {
             	StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+                bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+                bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
             }
 
 		}
