586581e2b1b3adbab3f46d02d2b265423b23a0d3
Xiaobo He <hexiaobo001@hotmail.com>
2020-03-23 14:56:03 -0400

    complete lab6

---
+++ lab6/UnionFindTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class UnionFindTest {
+    @Test
+    public void UFtest() {
+        UnionFind uf = new UnionFind(10);
+        uf.union(0,1);
+        uf.union(1,2);
+        uf.union(2,3);
+        uf.union(3,4);
+        assertEquals(5, uf.sizeOf(0));
+        assertEquals(5, uf.sizeOf(1));
+        assertEquals(5, uf.sizeOf(2));
+        assertEquals(5, uf.sizeOf(3));
+        assertTrue(uf.connected(0,3));
+    }
+
+
+
+    public static void main(String[] args) {
+        System.exit(jh61b.junit.textui.runClasses(UnionFindTest.class));
+    }
+}
